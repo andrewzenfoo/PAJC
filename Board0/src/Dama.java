f37b8e76d1663b3871690ebe6a87adc9d8ce0a31
char <char@zeon>
2016-11-29 19:23:46 +0100

    Added some conditions in move() method

--- Board0/src/Dama.java
+++ Board0/src/Dama.java
@@ -76,10 +76,53 @@ public class Dama {
 
 	public Boolean move(String idPezzo, int x, int y) {
 		Pezzo pezzo = listaPezzi.get(idPezzo);
+		Point posFinale = new Point(x, y);
+		ArrayList<Point> mosse = new ArrayList();
 		
-		if(pezzo != null) {
-			pezzo.posizione = new Point(x, y);
-			return true;
+		for(Pezzo p : listaPezzi()) {
+			if(pezzo.colore.equals(Colore.BIANCO)) {
+				if(pezzo.tipo.equals(Tipo.PEDINA)) {
+					mosse.add(new Point(pezzo.posizione.x+1, pezzo.posizione.y+1));
+					mosse.add(new Point(pezzo.posizione.x-1, pezzo.posizione.y+1));
+				}
+				else {
+					mosse.add(new Point(pezzo.posizione.x+1, pezzo.posizione.y+1));
+					mosse.add(new Point(pezzo.posizione.x-1, pezzo.posizione.y+1));
+					mosse.add(new Point(pezzo.posizione.x+1, pezzo.posizione.y-1));
+					mosse.add(new Point(pezzo.posizione.x-1, pezzo.posizione.y-1));
+				}
+			}
+			else {
+				if(pezzo.tipo.equals(Tipo.PEDINA)) {
+					mosse.add(new Point(pezzo.posizione.x+1, pezzo.posizione.y-1));
+					mosse.add(new Point(pezzo.posizione.x-1, pezzo.posizione.y-1));
+				}
+				else {
+					mosse.add(new Point(pezzo.posizione.x+1, pezzo.posizione.y+1));
+					mosse.add(new Point(pezzo.posizione.x-1, pezzo.posizione.y+1));
+					mosse.add(new Point(pezzo.posizione.x+1, pezzo.posizione.y-1));
+					mosse.add(new Point(pezzo.posizione.x-1, pezzo.posizione.y-1));					
+				}
+			}
+			
+		}
+		//Se selezione una casella dove non dovrebbe andare il pezzo si muove in una casella valida
+		if(((pezzo != null && pezzo.colore.equals(Colore.BIANCO) && posFinale.y > pezzo.posizione.y) ||		//Controllo che il pezzo non si muova all'indietro
+				(pezzo != null && pezzo.colore.equals(Colore.NERO) && posFinale.y < pezzo.posizione.y)) &&	//Controllo che il pezzo non si muova all'indietro
+				(posFinale.x > 0 && posFinale.x < size && posFinale.y > 0 && posFinale.y < size) &&			//Controllo che posFinale sia dentro la board
+				(posFinale.x < pezzo.posizione.x + 2 && posFinale.x > pezzo.posizione.x - 2)) {				
+			for(int i=0; i<mosse.size(); i++) {
+				if(!posFinale.equals(mosse.get(i))) {
+					mosse.remove(i);
+					//pezzo.posizione = new Point(x,y);
+					//return true;
+				}
+				//else 
+					//pezzo.posizione = new Point(x, y);
+				
+			}
+
+			pezzo.posizione = mosse.get(0).getLocation();
 		}
 		return false;
 	}
